e8b9407197b6a2ea6947238e81759466562d3d8a
Brandon Felty <brcfelty@gmail.com>
2022-03-25 09:01:48 -0600

    Use conditionals to advise on the weather

---
+++ conditionals.js
@@ -0,0 +1,26 @@
+const raining = true;
+const cold = false;
+
+if (raining) {
+   console.log("Don't forget your umbrella!");
+ }
+
+ /** For part 1
+ if (cold) {
+   console.log("Make sure to pick out a scarf!");
+ }
+
+ console.log("Now you're ready to go outside!");
+*/
+
+const temp = 12;
+
+ if (temp < 0) {
+   console.log("Make sure you pick out a scarf!");
+ } else if (temp < 15) {
+   console.log("Short sleeves won't cut it!");
+ } else {
+   console.log("Short sleeves are fine");
+ } 
+
+ console.log("Now you're ready to go outside!")
